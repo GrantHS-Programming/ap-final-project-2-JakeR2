34bdf95b4d3395846b15da628610f0ed6209c82a
Jake <jrobertson4803@student.pps.net>
2023-05-16 10:02:20 -0700

    secondle commit

--- Assets/ClownScript.cs
+++ Assets/ClownScript.cs
@@ -6,6 +6,9 @@ public class ClownScript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     public float jumpStrength;
+    public bool touchingPlatform = false;
+    public float moveStrength;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +18,25 @@ public class ClownScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (touchingPlatform)
         {
             myRigidbody.velocity = Vector2.up * jumpStrength;
         }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            myRigidbody.velocity = Vector2.right * moveStrength;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            myRigidbody.velocity = Vector2.left * moveStrength;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        touchingPlatform = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        touchingPlatform = false;
     }
 }
--- Assets/RedPlatformScript.cs
+++ Assets/RedPlatformScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class RedPlatformScript : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +16,5 @@ public class RedPlatformScript : MonoBehaviour
     {
         
     }
+    
 }
